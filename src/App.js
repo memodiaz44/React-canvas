187f9bb35237ee7d565ce32e37ca489b71ee1510
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-06-03 23:22:17 -0600

    h1 style commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Draw in here</h1>
+      <h1 className="h1">Draw in here</h1>
       <Canvas/>
     </div>
   );
